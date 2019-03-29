6efd3ee8c7ed9a6f8af9b373b938212fb3cf5648
jaqueytw <45243234+jaqueytw@users.noreply.github.com>
2019-03-29 17:30:36 -0300

    Update 5_haplotype_freq_micro.pl

--- Scripts_PaternityCalc/5_haplotype_freq_micro.pl
+++ Scripts_PaternityCalc/5_haplotype_freq_micro.pl
@@ -5,14 +5,13 @@
 
 #SCRIPT TO SEPARATE READS THAT HAD PASSED THE ALIGNMENT, MAPPING, BASE QUALITY, SNPS COVERAGE AND PERCENTAGE OF COVERED SNPS.
 
-#the script receives 7 parameters
-#1 - Bam file
-#2 - Mapping quality
-#3 - YesCIGAR or NotCIGAR
-#4 - Bases quality
-#5 - Percentage of covered bases
-#6 - Trio number
-#7 - Sample type
+#This script receives 6 parameters
+#1 - Mapping quality
+#2 - YesCIGAR or NotCIGAR
+#3 - Bases quality
+#4 - Percentage of covered bases
+#5 - Trio number
+#6 - Sample type
 
 ####################################################################################
 
@@ -20,7 +19,6 @@ use strict;
 use Getopt::Long;
 
 my $help = 0;
-my $BAM;
 my $map = 20;
 my $cigar = "NotCIGAR";
 my $score = 20;
@@ -29,7 +27,6 @@ my $trio;
 my $amostra;
 
 GetOptions("help|h" => \$help,
-	   "b=s" => \$BAM,
 	   "m=s" => \$map,
 	   "l=s" => \$cigar,
 	   "q=s" => \$score,
@@ -38,13 +35,12 @@ GetOptions("help|h" => \$help,
 	   "a=s" => \$amostra
     ) or die "Failed to take the options! \n";
 
-if ($help || !($BAM && $trio && $amostra)) {die "\
-This script requires three inputs, the bam file, the trio number and the sample type. \
+if ($help || !($trio && $amostra)) {die "\
+This script requires two inputs, the trio number and the sample type. \
 Other parameters have default values, but can be changed. \
 The output is a file with the haplotypes found in each microhaplotype.\
 \
 Required parameters:\
-	-b	Bam file\
 	-t	Trio number \
 	-a	Type of sample AF (alleged father), M (mother) ou P (plasma) \ 
 \
